e00bb98d00b9290982fe415232ea763fe81eb5a5
Nguyen Khanh <ngkhanh201198@gmail.com>
2021-12-07 16:56:09 +0700

    update question

--- src/main/java/com/backend/survey/dto/QuestionDTO.java
+++ src/main/java/com/backend/survey/dto/QuestionDTO.java
@@ -8,7 +8,7 @@ import java.util.List;
 public class QuestionDTO {
     private long questionId;
     private String question;
-    private List<AnswerDTO> listAnswer;
+        private List<AnswerDTO> listAnswer;
 
     public QuestionDTO() {
         super();
